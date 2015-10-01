6b44deee30d059b7a63bd7bbfc5cdf25424db0ef
nantharupan <nantha86@gmail.com>
2015-10-01 21:55:46 +0200

    Unwanted Files Removed
    Unwanted Projects rmoved

---
+++ MVC/Models/DefaultModelBinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Models
+{
+    public class ProfileModelBinder : DefaultModelBinder
+    {
+        protected override void OnModelUpdated(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var userProfileBase = bindingContext.Model as Movie;
+            if (userProfileBase != null)
+            {
+                userProfileBase.MovieName = controllerContext.HttpContext.User.Identity.Name;
+            }
+
+            base.OnModelUpdated(controllerContext, bindingContext);
+        }
+    }
+}
